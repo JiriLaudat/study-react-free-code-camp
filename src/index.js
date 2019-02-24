1513f800434c4862a57302a9ccc35d4f0dd12fe3
Jiří Laudát <jiri.laudat@actum.cz>
2019-02-24 14:24:34 +0100

    React DOM & JSX practice

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
 
+ReactDOM.render(
+  <ul>
+    <li>ham</li>
+    <li>cheese</li>
+    <li>bread</li>
+  </ul>,
+  document.getElementById('root'))
