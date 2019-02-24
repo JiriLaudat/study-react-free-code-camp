819af83b7728b292cfeafb4e38f3379ce3243d21
Jiří Laudát <jiri.laudat@actum.cz>
2019-02-24 19:04:38 +0100

    Todo App -Phase 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React from "react"
 
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-
 function App() {
     return (
-        <div>
-            <Header />
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum facere quas quisquam deserunt illo cumque consequatur aliquam sint nemo vel!</p>
-            <Footer />
-        </div>
+        <form>
+
+            <input type="checkbox" />
+            <p>option 1</p>
+
+            <input type="checkbox" />
+            <p>option 2</p>
+
+            <input type="checkbox" />
+            <p>option 3</p>
+
+            <input type="checkbox" />
+            <p>option 4</p>
+            
+        </form>
     )
 }
 
