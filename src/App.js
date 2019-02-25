08aa1da22de772380cb5396cf3de0213577fc6d5
Jiří Laudát <jiri.laudat@actum.cz>
2019-02-25 16:10:43 +0100

    Props in React

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from "react"
-import Header from "./components/Header"
-import TodoItem from "./components/TodoItem"
+import React from "react";
+import Header from "./components/Header";
+import TodoItem from "./components/TodoItem";
+import ContactCard from "./components/ContactCard";
 
 function App() {
     return (
@@ -15,8 +16,33 @@ function App() {
                 <TodoItem />
                 <TodoItem />
             </div>
+
+            <ContactCard
+                name="Mr. Whiskerson"
+                imgUrl="http://placekitten.com/300/200"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+            />
+            <ContactCard
+                name="Fluffykins"
+                imgUrl="http://placekitten.com/400/200"
+                phone="(212) 555-2345"
+                email="fluff@me.meow"
+            />
+            <ContactCard
+                name="Destroyer"
+                imgUrl="http://placekitten.com/400/300"
+                phone="(212) 555-3456"
+                email="ofworlds@yahoo.com"
+            />
+            <ContactCard
+                name="Felix"
+                imgUrl="http://placekitten.com/200/100"
+                phone="(212) 555-4567"
+                email="thecat@hotmail.com"
+            />
         </div>
     )
 }
 
-export default App
+export default App;
